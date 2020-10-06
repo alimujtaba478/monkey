db3afa81bedc68c803dab6bc091371d1ef1fa553
alimujtaba478 <70099666+alimujtaba478@users.noreply.github.com>
2020-10-06 14:43:41 +0500

    create static folder

--- monkey/monkey/settings.py
+++ monkey/monkey/settings.py
@@ -37,11 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-'django.contrib.sites',
+    'django.contrib.sites',
     'places',
-'rest_framework',
+    'rest_framework',
     'rest_framework.authtoken',
-
+    'storages',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -129,11 +129,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATIC_URL = '/static/'
+MEDIA_URL = "/media/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
+
